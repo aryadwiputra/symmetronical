f8bdea2832271670f5c3aa7f7ab2b9c7b8769567
Arya Dwi Putra <aryadptr.developer@gmail.com>
2024-10-28 08:51:11 +0700

    xlao

---
+++ resources/js/Components/ModalForm.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/Components/ui/dialog";
+import { Button } from "./ui/button";
+
+defineProps({
+    name: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger class="inline-flex w-1">
+            <Button> {{ name }} </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>{{ name }}</DialogTitle>
+                <DialogDescription>
+                    <slot />
+                </DialogDescription>
+            </DialogHeader>
+
+            <DialogFooter>
+                <Button type="submit"> Save changes </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
--- resources/js/Pages/Admin/Roles/Index.vue
+++ resources/js/Pages/Admin/Roles/Index.vue
@@ -1,13 +1,19 @@
 <script setup>
 import AppHeader from "@/Components/Dashboard/AppHeader.vue";
+import DataTable from "@/Components/Dashboard/DataTable.vue";
+import ModalForm from "@/Components/ModalForm.vue";
 import DashboardLayout from "@/Layouts/DashboardLayout.vue";
 
 defineOptions({
-    layout: DashboardLayout
-})
-
+    layout: DashboardLayout,
+});
 </script>
 
 <template>
     <AppHeader title="Roles" />
+    <ModalForm name="Create Role">
+        <form>
+        </form>
+    </ModalForm>
+    <DataTable />
 </template>
